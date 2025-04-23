776af7ccbbe0b8f10c762a22e3d619f98ce99217
bathant702 <bathant702nv@gmail.com>
2025-04-23 15:27:31 -0700

    removed the alert from the last lesson. got it back to normal where it is only the list for css.

--- components/ListGroup/ListGroup.tsx
+++ components/ListGroup/ListGroup.tsx
@@ -1,39 +1,16 @@
 import { useState } from "react";
+import "./ListGroup.css";
 
-//{ items:[], heading: string} for interface. this means we want to learn how to handle items in an array and display their name as a heading (string)
-interface Props {
-  items: string[];
-  heading: string;
-  onSelectItem: (item: string) => void;
-}
-
-function ListGroup({ items, heading, onSelectItem }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+const ListGroup = () => {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
-    <>
-      <h1>{heading}</h1>
-      {items.length === 0 && <p>No item found.</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
-            key={item}
-            onClick={() => {
-              setSelectedIndex(index);
-              onSelectItem(item);
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
   );
-}
+};
 
 export default ListGroup;
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { useState } from "react";
-import Alert from "../components/Alert";
-import Button from "../components/Button";
-import Test from "../components/Test";
-
+import ListGroup from "../components/ListGroup/ListGroup";
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hello</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        Click ME
-      </Button>
-
-      <Test></Test>
+      <ListGroup />
     </div>
   );
 }
